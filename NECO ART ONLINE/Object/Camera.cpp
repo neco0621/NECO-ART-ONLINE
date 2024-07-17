4ce63753aedb4e399f9e649f3e52afa80105e55b
neco0621 <kane263shun@icloud.com>
2024-07-17 18:30:33 +0900

    up

--- NECO ART ONLINE/Object/Camera.cpp
+++ NECO ART ONLINE/Object/Camera.cpp
@@ -22,19 +22,8 @@ Camera::~Camera()
 
 void Camera::Update(const Player& player)
 {
-#if !USE_LERP_CAMERA
 	// z軸上で、プレイヤーから一定距離離れた状態でプレイヤーを常に見続けるよう位置調整
 	m_pos = VGet(player.GetPos().x, player.GetPos().y + 200.0f, player.GetPos().z - 150.0f);
-#else
-	// lerpを使用して実装.
-	// lerp(VECTOR a, VECTOR b, float t)は
-	// answer = a + ((b-a) * t)
-	VECTOR aimPos = VAdd(player.GetPos(), VScale(player.GetDir(), -30.0f));
-	aimPos = VAdd(aimPos, VGet(0, 20.0f, 0));
-	VECTOR posToAim = VSub(aimPos, m_pos);
-	VECTOR scaledPosToAim = VScale(posToAim, 0.1f);
-	pos = VAdd(m_pos, scaledPosToAim);
-#endif
 	
 	// カメラに位置を反映.
 	SetCameraPositionAndTarget_UpVecY(m_pos, player.GetPos());
